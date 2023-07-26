38d8a3486ab0f4ca98adfd3f1bc62f5989db5df2
Honye <hongye.jun@qq.com>
2023-07-26 08:28:58 +0800

    build: 增加 Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,9 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
+    "plugin:@typescript-eslint/recommended",
+    // "prettier" put last
+    "prettier",
   ],
   "globals": {
     "Atomics": "readonly",
